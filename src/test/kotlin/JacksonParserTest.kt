fb328837856367b8f59a9acb0f4eda6a3f0361c3
hans-jacob Melby <hans-jacob.melby@nav.no>
2023-08-03 10:18:32 +0200

    mer forklarende tester

--- src/test/kotlin/JacksonParserTest.kt
+++ src/test/kotlin/JacksonParserTest.kt
@@ -4,27 +4,18 @@ import org.junit.jupiter.api.Test
 
 class JacksonParserTest {
     @Test
-    fun `mapping til request objekt for status Ja med ikke norsk statsborger`(){
+    fun `mapping til JsonNode og tilbake til domene objekt igjen`(){
         val kn = KundeNavn("1","ole olesen")
         val json = JacksonParser().ToJson(kn)
         val parsed = JacksonParser().toDomainObject<KundeNavn>(json.toPrettyString())
         Assertions.assertNotNull(parsed)
     }
     @Test
-    fun `mapping `(){
-        val kn = KundeNavn("1","ole olesen")
-        val json = JacksonParser().ToJson(kn)
-        val parsed = JacksonParser().toDomainObject<KundeNavn>(json)
-        Assertions.assertNotNull(parsed)
-    }
-    @Test
-    fun `mapping2 `(){
+    fun `mapping fra kunde id til mer komplekst kundedto med test av sammenlikning `(){
         val kundeID = KundeID("1234")
         val node= JacksonParser().ToJson(kundeID)
         val kundeDTO:KundeDTO = JacksonParser().toDomainObject(node)
         val kn = KundeDTO("1","ole olesen","adresse","1234",KundeStatus.STANDARD)
-        if (kundeDTO.kundeStatus != KundeStatus.STANDARD){
-            println("test")
-        }
+        Assertions.assertTrue(kundeDTO.kundeStatus != KundeStatus.STANDARD)
     }
 }
